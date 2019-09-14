dd9b561879021bd886a3daa2ed49f72dc71819fc
Fernando Limón Flores <fernandolimonflores@MacBook-Pro-de-Fernando.local>
2019-09-14 18:26:58 -0500

    first commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Fernando Limón Flores on 8/18/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
